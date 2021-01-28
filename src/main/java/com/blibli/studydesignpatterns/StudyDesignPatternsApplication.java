4f975da4d5c79b6aa3f32e5644cd78ea47b6294c
tommylohil <tommylohil23052000@gmail.com>
2021-01-28 10:09:09 +0700

    Initial Commit

---
+++ src/main/java/com/blibli/studydesignpatterns/StudyDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.blibli.studydesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDesignPatternsApplication.class, args);
+	}
+
+}
